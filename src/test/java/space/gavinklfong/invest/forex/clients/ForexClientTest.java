313cac194b3bb1532184c4fac585f62ec1604bbe
Gavin Fong <gavinklfong@gmail.com>
2021-12-30 02:01:55 +0000

    added new test scenario

--- src/test/java/space/gavinklfong/invest/forex/clients/ForexClientTest.java
+++ src/test/java/space/gavinklfong/invest/forex/clients/ForexClientTest.java
@@ -3,6 +3,7 @@ package space.gavinklfong.invest.forex.clients;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -16,19 +17,21 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import space.gavinklfong.invest.InvestApplication;
 import space.gavinklfong.invest.forex.dtos.ForexRate;
+import space.gavinklfong.invest.forex.dtos.ForexRateBooking;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static java.util.Arrays.asList;
+import static space.gavinklfong.invest.forex.clients.TestConstants.*;
 
 @SpringBootTest(classes = {InvestApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -65,20 +68,12 @@ public class ForexClientTest {
 
     @Test
     void givenLatestRates_whenGetLatestRates_thenReturnResult() throws JsonProcessingException {
-
-        final List<ForexRate> FOREX_RATES = asList(
-                ForexRate.builder().baseCurrency("GBP")
-                        .counterCurrency("USD")
-                        .buyRate(1.1)
-                        .sellRate(1.2)
-                        .timestamp(LocalDateTime.now())
-                        .build());
-
         // Given
         wireMockRule.stubFor(get(urlEqualTo("/rates/latest"))
                 .willReturn(aResponse()
                         .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(objectMapper.writeValueAsString(FOREX_RATES)))
+                        .withBody(objectMapper.writeValueAsString(FOREX_RATES))
+                )
         );
 
         // When
@@ -87,7 +82,76 @@ public class ForexClientTest {
         // Then
         StepVerifier.create(rates)
                 .expectNext(FOREX_RATES.get(0))
+                .expectNext(FOREX_RATES.get(1))
                 .expectComplete();
+    }
+
+    @Test
+    void givenRateAvailableForBooking_whenSubmitBookingReq_thenReturnBooking() throws JsonProcessingException {
+        // Given
+        wireMockRule.stubFor(post(urlEqualTo("/rates/book"))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(RATE_BOOKING_REQ)))
+                .willReturn(aResponse()
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(objectMapper.writeValueAsString(RATE_BOOKING))
+                )
+        );
+
+        // When
+        Mono<ForexRateBooking> rateBooking = forexClient.bookRate(RATE_BOOKING_REQ);
+
+        // Then
+        StepVerifier.create(rateBooking)
+                .expectNext(RATE_BOOKING)
+                .verifyComplete();
+    }
+
+    @Test
+    void givenAllAttemptTimeout_whenSubmitBookingReq_thenReturnError() throws JsonProcessingException {
+        // Given
+        wireMockRule.stubFor(post(urlEqualTo("/rates/book"))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(RATE_BOOKING_REQ)))
+                .willReturn(aResponse()
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(objectMapper.writeValueAsString(RATE_BOOKING))
+                        .withFixedDelay(5000)
+                ));
+
+        // When
+        Mono<ForexRateBooking> rateBooking = forexClient.bookRate(RATE_BOOKING_REQ);
+
+        // Then
+        StepVerifier.create(rateBooking)
+                .expectError();
+    }
+
+    @Test
+    void given1stAnd2ndAttempTimeoutAnd3rdAttemptSuccess_whenSubmitBookingReq_thenReturnRateBooking() throws JsonProcessingException {
+        // Given
+        setupStubForRateBooking("retry", STARTED, "1st request received", 5000);
+        setupStubForRateBooking("retry", "1st request received", "2nd request received", 5000);
+        setupStubForRateBooking("retry", "2nd request received", "3rd request received", 1000);
+
+        // When
+        Mono<ForexRateBooking> rateBooking = forexClient.bookRate(RATE_BOOKING_REQ);
+
+        // Then
+        StepVerifier.create(rateBooking)
+                .expectNext(RATE_BOOKING)
+                .verifyComplete();
+    }
+
+    private void setupStubForRateBooking(String scenario, String currentState, String nextState, Integer fixedDelay) throws JsonProcessingException {
+        wireMockRule.stubFor(post(urlEqualTo("/rates/book"))
+                .inScenario(scenario)
+                .whenScenarioStateIs(currentState)
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(RATE_BOOKING_REQ)))
+                .willReturn(aResponse()
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(objectMapper.writeValueAsString(RATE_BOOKING))
+                        .withFixedDelay(fixedDelay)
+                )
+                .willSetStateTo(nextState));
     }
 
 }
---
+++ src/test/java/space/gavinklfong/invest/forex/clients/TestConstants.java
@@ -0,0 +1,51 @@
+package space.gavinklfong.invest.forex.clients;
+
+import space.gavinklfong.invest.forex.dtos.ForexRate;
+import space.gavinklfong.invest.forex.dtos.ForexRateBooking;
+import space.gavinklfong.invest.forex.dtos.ForexRateBookingReq;
+import space.gavinklfong.invest.forex.dtos.TradeAction;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.Arrays.asList;
+
+public interface TestConstants {
+
+    ForexRate GBP_USD_RATE =  ForexRate.builder().baseCurrency("GBP")
+            .counterCurrency("USD")
+            .buyRate(1.1)
+            .sellRate(1.2)
+            .timestamp(LocalDateTime.now())
+            .build();
+
+    ForexRate GBP_EUR_RATE =  ForexRate.builder().baseCurrency("GBP")
+            .counterCurrency("EUR")
+            .buyRate(0.95)
+            .sellRate(1.19)
+            .timestamp(LocalDateTime.now())
+            .build();
+
+    List<ForexRate> FOREX_RATES = asList(GBP_USD_RATE, GBP_EUR_RATE);
+
+    ForexRateBookingReq RATE_BOOKING_REQ = ForexRateBookingReq.builder()
+            .baseCurrency("GBP")
+            .counterCurrency("USD")
+            .customerId(1L)
+            .tradeAction(TradeAction.BUY)
+            .build();
+
+    ForexRateBooking RATE_BOOKING = ForexRateBooking.builder()
+            .bookingRef(UUID.randomUUID().toString())
+            .baseCurrency(RATE_BOOKING_REQ.getBaseCurrency())
+            .counterCurrency(RATE_BOOKING_REQ.getCounterCurrency())
+            .customerId(RATE_BOOKING_REQ.getCustomerId())
+            .tradeAction(RATE_BOOKING_REQ.getTradeAction())
+            .timestamp(LocalDateTime.now())
+            .id(1l)
+            .expiryTime(LocalDateTime.now().plusMinutes(15))
+            .rate(1.25)
+            .build();
+}
